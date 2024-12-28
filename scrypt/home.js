e869b273b7c2491c1f941062da8fcc2cb4efb907
sorayaskavinski <158097646+sorayaskavinski@users.noreply.github.com>
2024-12-27 21:47:14 -0300

    Add files via upload

---
+++ scrypt/home.js
@@ -0,0 +1,6 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.getElementById("hamburger").addEventListener("click", function() {
+    const nav = document.querySelector("nav ul");
+    nav.classList.toggle("visible");
+});
